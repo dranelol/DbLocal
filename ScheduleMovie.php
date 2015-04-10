4bb0b459c61afec45e74aacaad63a6edfcb8dbc9
ryanadair009 <ryan.adair009@gmail.com>
2015-04-09 20:05:32 -0500

    ScheduleMoviePHPScript

---
+++ ScheduleMovie.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+<title> 
+Schedule Movie
+</title>
+</head>
+
+<h3>Schedule Movie</h3>
+<br>
+<!-- <form action="cmps460server.cacs.louisiana.edu/rma5149/AcctChQuery.php" method="POST"> -->
+Complex 
+<select name="complex_select_menu">
+	<option value="complex1">Complex 1</option>
+	<option value="complex2">Complex 2</option>
+	<option value="complex3">Complex 3</option>
+</select>
+<br>
+<br>
+
+Movie 
+<select name="movie_select_menu">
+	<option value="thor">Thor</option>
+	<option value="avengers">The Avengers</option>
+	<option value="buttsekssisters">Buttseks Sisters 9</option>
+</select>
+<br>
+<br>
+
+Theater 
+<select name="theater_select_menu">
+	<option value="theater1">1</option>
+	<option value="theater2">2</option>
+	<option value="theater3">3</option>
+</select>
+<br>
+<br>
+
+Date
+<select name="date_select_menu">
+	<option value="today">Today</option>
+	<option value="today+1">Today + 1</option>
+	<option value="today+2">Today + 2</option>
+	<option value="today+3">Today + 3</option>
+	<option value="today+4">Today + 4</option>
+	<option value="today+5">Today + 5</option>
+	<option value="today+6">Today + 6</option>
+</select>
+<br>
+<br>
+
+Time
+<select name="time_select_menu">
+	<option value="00:00">12:00</option>
+	<option value="00:30">12:30</option>	
+	
+	<option value="01:00">01:00</option>
+	<option value="01:30">01:30</option>
+	
+	<option value="02:00">02:00</option>
+	<option value="02:30">02:30</option>	
+	
+	<option value="03:00">03:00</option>
+	<option value="03:30">03:30</option>	
+	
+	<option value="04:00">04:00</option>
+	<option value="04:30">04:30</option>	
+	
+	<option value="05:00">05:00</option>
+	<option value="05:30">05:30</option>	
+	
+	<option value="06:00">06:00</option>
+	<option value="06:30">06:30</option>	
+	
+	<option value="07:00">07:00</option>
+	<option value="07:30">07:30</option>	
+	
+	<option value="08:00">08:00</option>
+	<option value="08:30">08:30</option>	
+	
+	<option value="09:00">09:00</option>
+	<option value="09:30">09:30</option>	
+	
+	<option value="10:00">10:00</option>
+	<option value="10:30">10:30</option>	
+	
+	<option value="11:00">11:00</option>
+	<option value="11:30">11:30</option>
+</select>
+<select name="ampm_select_menu">
+	<option value="am">AM</option>
+	<option value="pm">PM</option>
+</select>
+<br>
+<br>
+
+<button type="button">Submit</button>
+
+</form>
+</body>
+</html>
