3eed0ac163fdee479d8bf5b4f49c2ac64cd1e8f4
dranelol <cardsfan853@gmail.com>
2015-04-20 18:09:54 -0500

    committing skeleton code for chu

---
+++ skeleton.php
@@ -0,0 +1,63 @@
+<?php
+
+	include "login.php";
+
+	if(isset($_SESSION["userType"]) == false)
+	{	
+		echo "Not logged in!";
+		echo '<br><br>';
+		echo '<a href ="LoginPage.php">Go Log In</a>';
+		
+		die();
+	}
+	
+	
+?>
+<html>
+<head>
+<title> 
+TITLE
+</title>
+</head>
+
+<h3>
+HEADING
+</h3>
+<body>
+
+<?php 
+
+	$sessionUser = $_SESSION['userType'];
+	
+	// restrict access only to certain userTypes
+	
+	/*
+	if($sessionUser != "member")
+	{
+		echo 'Not logged in as a member!';
+		echo '<br><br>';
+		echo '<a href ="LoginPage.php">Go Log In</a>';
+		
+		die();
+	}
+	*/
+
+	echo "Logged in as: $sessionUser"; 
+	
+	$date = $_SESSION["today"];
+	echo "<br>";
+	echo "Today's date: $date";
+	
+?> 
+
+<br>
+<br>
+
+<?php 
+
+	// CODE STARTS HERE
+	
+?>
+
+</body>
+</html>
